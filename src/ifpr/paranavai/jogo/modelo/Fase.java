5df0cc0546cd0122fdac4eb92b8b423339145997
Carla <carlasantos45@hotmail.com.br>
2023-05-23 22:46:34 -0300

    Criação da classe Fase e da pasta recursos

---
+++ src/ifpr/paranavai/jogo/modelo/Fase.java
@@ -0,0 +1,12 @@
+package ifpr.paranavai.jogo.modelo;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Fase {
+    private Image fundo;
+
+    public Fase(){
+        ImageIcon carregando = new ImageIcon("");
+    }
+}
